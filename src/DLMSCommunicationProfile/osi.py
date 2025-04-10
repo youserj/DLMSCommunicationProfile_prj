b5b6e51debf2c9cd32e7ba72257949a7f9b0a38f
vasilsv <youserj@mail.ru>
2025-04-10 13:28:24 +0300

    * ver 0.1.5 * OSI: order for str represent * OSI.str: empty for NONE

--- src/DLMSCommunicationProfile/osi.py
+++ src/DLMSCommunicationProfile/osi.py
@@ -1,18 +1,18 @@
-from enum import IntFlag, auto
+from enum import IntFlag
 
 
 class OSI(IntFlag):
+    APPLICATION = 0b1000000
+    PRESENTATION = 0b100000
+    SESSION = 0b10000
+    TRANSPORT = 0b1000
+    NETWORK = 0b100
+    DATA_LINK = 0b10
+    PHYSICAL = 0b1
     NONE = 0
-    PHYSICAL = auto()
-    DATA_LINK = auto()
-    NETWORK = auto()
-    TRANSPORT = auto()
-    SESSION = auto()
-    PRESENTATION = auto()
-    APPLICATION = auto()
 
     def __str__(self):
-        return self.name
+        return "" if self == 0 else self.name
 
     def __sub__(self, other):
         return self.__class__(super(OSI, self).__sub__(other))
